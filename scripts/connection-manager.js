6b9491d1a79ef76896744688c20ee9ef2dda350e
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-06 21:34:15 +0900

    wrote connection manager, server; installed WebSocketServer

---
+++ scripts/connection-manager.js
@@ -0,0 +1,17 @@
+class ConnectionManager 
+{
+    constructor() {
+        this.conn = null;
+    }
+
+    connect(address)
+    {
+        this.conn = new WebSocket(address);
+
+        this.conn.addEventListener('open', () => {
+            console.log('Connection established');
+
+            this.conn.send('create-session');
+        });
+    }
+}
